edb68dc6a1acff05f17948b4532dff99edb7c962
Dani Latorre <dani@danilat.com>
2022-06-16 00:06:45 +0200

    minor fixes in texts and in https urls

--- js/default.js
+++ js/default.js
@@ -9,7 +9,7 @@ WebFont.load({
 
 $(document).ready(function(){
   // Map
-  var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+  var layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>'
   });
   var map = L.map('google-map', {
